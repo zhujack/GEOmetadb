398890d31f60d597e55dfb767d8aab46e896bd56
j.zhu <zhujack@mail.nih.gov>
2020-04-09 14:53:03 -0400

    created a demo database for downloading

--- R/getSQLiteFile.R
+++ R/getSQLiteFile.R
@@ -1,7 +1,12 @@
 getSQLiteFile <-
-function(destdir=getwd(),destfile='GEOmetadb.sqlite.gz') {
+function(destdir=getwd(),destfile='GEOmetadb.sqlite.gz',type='full') {
   localfile <- file.path(destdir,destfile)
-  url_geo = "http://starbuck1.s3.amazonaws.com/sradb/GEOmetadb.sqlite.gz"
+  if(type == 'full') {
+      url_geo = "http://starbuck1.s3.amazonaws.com/sradb/GEOmetadb.sqlite.gz"
+  } else {
+      url_geo = "http://starbuck1.s3.amazonaws.com/sradb/GEOmetadb_demo.sqlite.gz"
+  }
+  
 
   download.file(url_geo, destfile=localfile,mode='wb')
   cat('Unzipping...\n')
